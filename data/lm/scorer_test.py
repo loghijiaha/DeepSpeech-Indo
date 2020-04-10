80c88c62614fbf2ab94152302b210dbd43cedd21
Loghi <loghijiaha.16@cse.mrt.ac.lk>
2020-04-11 00:49:02 +0530

    Update scorer_test.py

--- data/lm/scorer_test.py
+++ data/lm/scorer_test.py
@@ -2,6 +2,5 @@ import os
 from ds_ctcdecoder import ctc_beam_search_decoder_batch, Scorer
 from util.text import Alphabet, UTF8Alphabet
 alphabet = UTF8Alphabet()
-scorer = Scorer(0.75,1.85,
-                        "data/lm/kenlm_char.scorer", alphabet)
+scorer = Scorer(0.75,1.85,"/content/DeepSpeech-Indo/data/lm/kenlm_tamil.scorer", alphabet)
 print(scorer)
